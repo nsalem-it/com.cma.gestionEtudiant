b89bac117c4d86f84149fb15d411db3165f872d1
nsalem-it <nasreddine.salem@yahoo.fr>
2021-09-04 11:26:13 -0400

    Initial commit

---
+++ src/test/java/com/cma/gestionEtudiant/GestionEtudiantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cma.gestionEtudiant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEtudiantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
